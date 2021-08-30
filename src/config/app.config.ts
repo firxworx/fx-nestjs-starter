7a6c40421bf742aaf165c0e30bac5c3c6af55b69
Kevin Firko <kfirko@bitcurve.com>
2021-08-30 00:56:29 -0400

    initial app config + defaults

---
+++ src/config/app.config.ts
@@ -0,0 +1,54 @@
+import { registerAs } from '@nestjs/config'
+import {
+  DEFAULT_PORT,
+  DEFAULT_API_VERSION,
+  DEFAULT_BASE_PATH,
+  DEFAULT_THROTTLE_LIMIT,
+  DEFAULT_THROTTLE_TTL,
+} from './defaults'
+
+const PORT = process.env.PORT ? +process.env.PORT : DEFAULT_PORT
+
+/**
+ * Return the CORS origin for this environment per `NODE_ENV`.
+ *
+ * Reminder: CORS + cookies requires a complete url that includes ports.
+ *
+ * @throws if process.env.ORIGIN is not defined when `NODE_ENV` production.
+ */
+const getOrigin = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return process.env.ORIGIN ?? `http://localhost:${PORT}`
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    if (!process.env.ORIGIN) {
+      throw new Error('ORIGIN environment variable must be defined in production')
+    }
+
+    return process.env.ORIGIN
+  }
+
+  // fallback to wildcard suitable for other potential use-cases in dev + test environments
+  return '*'
+}
+
+export interface AppConfig {
+  origin: string
+  port: number
+  basePath: string
+  apiVersion: string
+  throttleTTL: number
+  throttleLimit: number
+}
+
+export default registerAs('app', (): AppConfig => {
+  return {
+    origin: getOrigin(),
+    port: PORT,
+    basePath: process.env.BASE_PATH ?? DEFAULT_BASE_PATH,
+    apiVersion: process.env.API_VERSION ?? DEFAULT_API_VERSION,
+    throttleTTL: process.env.THROTTLE_TTL ? +process.env.THROTTLE_TTL : DEFAULT_THROTTLE_TTL,
+    throttleLimit: process.env.THROTTLE_LIMIT ? +process.env.THROTTLE_LIMIT : DEFAULT_THROTTLE_LIMIT,
+  }
+})
---
+++ src/config/defaults.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_PORT = 3000
+export const DEFAULT_BASE_PATH = '/'
+
+/** API version (e.g. 'v1') to use in REST URL path. Refer to the global prefix set in `main.ts`. */
+export const DEFAULT_API_VERSION = 'v1'
+
+export const DEFAULT_THROTTLE_LIMIT = 25
+export const DEFAULT_THROTTLE_TTL = 60

80dac71136ea1fae1b0bdfd46d12f5df0b1cbe55
Kevin Firko <kfirko@bitcurve.com>
2021-08-30 01:14:43 -0400

    add ConfigModule to global app module import so it is available to any module in the project

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import appConfig from 'src/config/app.config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    // @see - https://docs.nestjs.com/techniques/configuration
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

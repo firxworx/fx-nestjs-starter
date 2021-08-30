baeb2c2402fe89aec1f0a214fe0c57ba89f177f3
Kevin Firko <kfirko@bitcurve.com>
2021-08-30 01:16:12 -0400

    add doc comments to defaults

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -1,4 +1,7 @@
+/** Default port (note: NestJS default is 3000). */
 export const DEFAULT_PORT = 3000
+
+/** Base path of this API. A path other than '/' may be required if this API is not at domain/subdomain root. */
 export const DEFAULT_BASE_PATH = '/'
 
 /** API version (e.g. 'v1') to use in REST URL path. Refer to the global prefix set in `main.ts`. */

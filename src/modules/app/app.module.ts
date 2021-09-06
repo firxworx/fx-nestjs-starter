32cfadd1fd93cc96f600bdfc55d515eafdc8aecc
Kevin Firko <kfirko@bitcurve.com>
2021-09-06 13:48:57 -0400

    add authConfig w/ JWT values + add to app module imports

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import appConfig from 'src/config/app.config'
+import authConfig from 'src/config/auth.config'
+// import { DatabaseModule } from '../database/database.module'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
@@ -10,8 +12,9 @@ import { AppService } from './app.service'
     // @see - https://docs.nestjs.com/techniques/configuration
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig],
+      load: [appConfig, authConfig],
     }),
+    // DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],

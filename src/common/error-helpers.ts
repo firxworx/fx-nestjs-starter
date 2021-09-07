9985fd80017bf5bbd297f71a2f037212f2ad5959
Kevin Firko <kfirko@bitcurve.com>
2021-09-06 21:29:09 -0400

    add getErrorStack() helper

--- src/common/error-helpers.ts
+++ src/common/error-helpers.ts
@@ -10,3 +10,16 @@
 export const getErrorMessage = (error: unknown) => {
   return (error && error instanceof Error && error.message) || String(error)
 }
+
+/**
+ * Return the error stack (or `undefined`) from the given error of type `unknown`.
+ *
+ * Returns the error `stack` property value if the given error is an instance of `Error`.
+ *
+ * Intended for use in a catch blocks and with logging functions.
+ *
+ * @param error unknown error
+ */
+export const getErrorStack = (error: unknown) => {
+  return (error && error instanceof Error && error.stack) || undefined
+}

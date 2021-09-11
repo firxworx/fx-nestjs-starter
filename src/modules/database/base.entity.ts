5b0ed101581df70cd7537b87429f2c852f9cec1a
Kevin Firko <kfirko@bitcurve.com>
2021-09-11 14:12:28 -0400

    add isActive flag to Base database entity and commented-out suggestions for additional default columns

--- src/modules/database/base.entity.ts
+++ src/modules/database/base.entity.ts
@@ -11,12 +11,24 @@ export abstract class Base {
   @Column()
   readonly uuid!: string
 
-  @CreateDateColumn()
+  @CreateDateColumn() // @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   readonly createdAt!: Date
 
-  @UpdateDateColumn()
+  @UpdateDateColumn() // @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   readonly updatedAt!: Date
 
-  // createdBy
-  // updatedBy
+  // @Column({ type: 'varchar', length: 300 })
+  // createdBy: string
+
+  // @Column({ type: 'varchar', length: 300 })
+  // updatedBy: string
+
+  // @Column({ type: 'varchar', length: 300, nullable: true })
+  // internalComment: string | null
+
+  @Column({ type: 'boolean', default: true })
+  isActive!: boolean
+
+  // @Column({ type: 'boolean', default: false })
+  // isArchived!: boolean
 }

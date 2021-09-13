c9102d1afce325a30cd496739221ea126a53e1ac
Kevin Firko <kfirko@bitcurve.com>
2021-09-13 01:39:24 -0400

    revise PaginatedResponseDto to work around TypeScript reflection limitations + generics with class-transformer - accept class as constructor arg + use for Type() decorator

--- src/modules/database/dto/paginated-response.dto.ts
+++ src/modules/database/dto/paginated-response.dto.ts
@@ -1,12 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { Exclude, Type } from 'class-transformer'
 import { IsArray, IsNumber } from 'class-validator'
 
 /**
  * Response DTO for paginated responses that includes a `data` array of type `T` and a `totalCount`.
  */
 export class PaginatedResponseDto<T> {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(type: Function, data: Array<T>, totalCount: number) {
+    this.type = type
+    this.data = data
+    this.totalCount = totalCount
+  }
+  // constructor(private type: { new (): T }) {}
+
+  @Exclude()
+  private type: Function // eslint-disable-line @typescript-eslint/ban-types
+  // private type: new (): T
+
   @ApiProperty({ isArray: true })
   @IsArray()
+  @Type((options) => {
+    return (options?.newObject as PaginatedResponseDto<T>)?.type
+  })
   readonly data!: Array<T>
 
   @ApiProperty({

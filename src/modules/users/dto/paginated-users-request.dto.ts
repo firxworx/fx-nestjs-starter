a58914afd07e96024e38633838ff6baa9edaa3b6
Kevin Firko <kfirko@bitcurve.com>
2021-09-30 01:23:02 -0400

    remove paginated-users-request

--- src/modules/users/dto/paginated-users-request.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PaginationParamsAbstractDto } from '../../database/dto/pagination-params.abstract.dto'
-import { User } from '../entities/user.entity'
-
-export class PaginatedUsersRequestDto extends PaginationParamsAbstractDto<User> {}

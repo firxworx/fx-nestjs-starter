8018d35618ed95b3893b03741590b910a1737733
Kevin Firko <kfirko@bitcurve.com>
2021-10-02 18:54:43 -0400

    stripe module + StripeService scaffolding w/ DynamicModule registration for reusability

--- src/modules/stripe/stripe.module.ts
+++ src/modules/stripe/stripe.module.ts
@@ -1,54 +1,45 @@
 import { DynamicModule, Module, Provider } from '@nestjs/common'
 import { DiscoveryModule } from '@nestjs/core'
-import Stripe from 'stripe'
 
 import { STRIPE_CLIENT, STRIPE_MODULE_OPTIONS } from './stripe.constants'
 import { StripeWebhookService } from './stripe-webhook.service'
-import { createStripeProvider } from './stripe.provider'
+import { createStripeClientProvider } from './stripe-client.provider'
 import { StripeModuleAsyncOptions } from './types/stripe-module-async-options.interface'
 import { StripeModuleOptions } from './types/stripe-module-options.interface'
 import { StripeOptionsFactory } from './types/stripe-options-factory.interface'
 import { StripeService } from './stripe.service'
+import { ConfigModule } from '@nestjs/config'
 
-// https://stackoverflow.com/questions/65704597/nestjs-nest-cant-resolve-dependencies-of-the-usersservice
-// https://github.com/ssut/nestjs-sqs/blob/master/lib/sqs.module.ts
-
-@Module({
-  imports: [DiscoveryModule],
-  providers: [StripeService, StripeWebhookService],
-  exports: [StripeService, StripeWebhookService],
-})
+// @Global()
+@Module({})
 export class StripeModule {
-  static register(options: StripeModuleOptions): DynamicModule {
-    const stripeProvider = createStripeProvider(options)
-
+  public static register(options: StripeModuleOptions): DynamicModule {
     return {
       module: StripeModule,
       global: options.global ?? false,
-      imports: [DiscoveryModule],
-      // exports: [Stripe, StripeService, StripeWebhookService],
+      imports: [DiscoveryModule, ConfigModule],
+      providers: [
+        createStripeClientProvider(),
+        { provide: STRIPE_MODULE_OPTIONS, useValue: options },
+        StripeService,
+        StripeWebhookService,
+      ],
       exports: [StripeService, StripeWebhookService],
-      providers: [stripeProvider, StripeService, StripeWebhookService],
     }
   }
 
-  static registerAsync(options: StripeModuleAsyncOptions): DynamicModule {
-    const stripeProvider: Provider = {
-      // provide: Stripe,
-      provide: STRIPE_CLIENT,
-      inject: [STRIPE_MODULE_OPTIONS],
-      useFactory: (stripeModuleOptions: StripeModuleOptions) => {
-        return new Stripe(stripeModuleOptions.apiKey, stripeModuleOptions.stripeConfig)
-      },
-    }
-
+  public static registerAsync(options: StripeModuleAsyncOptions): DynamicModule {
     return {
       module: StripeModule,
       global: options.global ?? false,
-      imports: [DiscoveryModule, ...(options.imports ?? [])],
-      // exports: [Stripe, StripeService, StripeWebhookService],
-      exports: [StripeService, StripeWebhookService],
-      providers: [StripeService, StripeWebhookService, stripeProvider, ...this.createAsyncProviders(options)],
+      imports: [DiscoveryModule, ConfigModule, ...(options.imports ?? [])],
+      providers: [
+        createStripeClientProvider(),
+        ...this.createAsyncProviders(options),
+        StripeService,
+        StripeWebhookService,
+      ],
+      exports: [StripeService, StripeWebhookService, STRIPE_CLIENT],
     }
   }
 
--- src/modules/stripe/stripe.service.ts
+++ src/modules/stripe/stripe.service.ts
@@ -1,19 +1,28 @@
 import { Inject, Injectable, Scope } from '@nestjs/common'
 import Stripe from 'stripe'
-import { STRIPE_CLIENT, STRIPE_MODULE_OPTIONS } from './stripe.constants'
-import { StripeModuleOptions } from './types/stripe-module-options.interface'
-
-// @Inject(STRIPE_MODULE_OPTIONS) public readonly options: StripeModuleOptions
+import { STRIPE_CLIENT } from './stripe.constants'
 
 @Injectable({
   scope: Scope.DEFAULT,
 })
 export class StripeService {
-  // constructor(private readonly stripeClient: Stripe) {}
-  constructor(@Inject(STRIPE_CLIENT) private readonly stripeClient: Stripe) {}
-  buh = (): any => {
-    return {
-      hello: 'hi',
-    }
+  constructor(
+    @Inject(STRIPE_CLIENT) private readonly stripeClient: Stripe, // @Inject(STRIPE_MODULE_OPTIONS) private stripeModuleOptions: StripeModuleOptions,
+  ) {}
+
+  async getCustomers() {
+    return this.stripeClient.customers.list({})
+  }
+
+  async getCustomerBalance(stripeCustomerId: string) {
+    return this.stripeClient.customers.retrieve(stripeCustomerId, { expand: ['balances'] })
+  }
+
+  async createStripeCustomer(name: string) {
+    const customer = await this.stripeClient.customers.create({
+      name,
+    })
+
+    return { stripeCustomerId: customer.id }
   }
 }

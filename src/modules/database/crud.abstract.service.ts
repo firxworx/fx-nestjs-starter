50ca2d0f4293406ecb0b7748051cacdfd8c8aa0b
Kevin Firko <kfirko@bitcurve.com>
2021-11-08 09:54:21 -0500

    add support for both findOne() and find() to CrudAbstractService

--- src/modules/database/crud.abstract.service.ts
+++ src/modules/database/crud.abstract.service.ts
@@ -1,16 +1,56 @@
-import { NotFoundException } from '@nestjs/common'
-import { DeepPartial, Repository } from 'typeorm'
+import { InternalServerErrorException, NotFoundException } from '@nestjs/common'
+import { DeepPartial, In, Repository } from 'typeorm'
 import { CrudService } from './types/crud-service.interface'
 import { BaseAbstractEntity } from './base.abstract.entity'
 
+const isNumberArray = (value: unknown): value is Array<number> => {
+  if (!Array.isArray(value)) {
+    return false
+  }
+
+  if (value.some((v) => typeof v !== 'number')) {
+    return false
+  }
+
+  return true
+}
+
+const isStringArray = (value: unknown): value is Array<string> => {
+  if (!Array.isArray(value)) {
+    return false
+  }
+
+  if (value.some((v) => typeof v !== 'string')) {
+    return false
+  }
+
+  return true
+}
+
 export abstract class CrudAbstractService<T extends BaseAbstractEntity> implements CrudService<T> {
-  protected constructor(protected readonly genericRepository: Repository<T>) {}
+  protected constructor(protected genericRepository: Repository<T>) {}
 
   async findAll(): Promise<Array<T>> {
     return this.genericRepository.find()
   }
 
-  async find<ID extends number | string>(identifier: ID): Promise<T | undefined> {
+  async find<ID extends Array<number> | Array<string>>(identifiers: ID): Promise<Array<T>> {
+    if (isNumberArray(identifiers)) {
+      return this.genericRepository.find({
+        where: { id: In(identifiers) },
+      })
+    }
+
+    if (isStringArray(identifiers)) {
+      return this.genericRepository.find({
+        where: { uuid: In(identifiers) },
+      })
+    }
+
+    throw new InternalServerErrorException('Entity exception')
+  }
+
+  async findOne<ID extends number | string>(identifier: ID): Promise<T | undefined> {
     if (typeof identifier === 'string') {
       return this.genericRepository.findOne({
         where: {
@@ -30,7 +70,12 @@ export abstract class CrudAbstractService<T extends BaseAbstractEntity> implemen
   }
 
   async update(identifier: number | string, dto: DeepPartial<T>): Promise<T> {
-    const id = typeof identifier === 'number' ? identifier : (await this.find(identifier))?.id
+    const id = typeof identifier === 'number' ? identifier : (await this.findOne(identifier))?.id
+
+    if (!id) {
+      throw new NotFoundException(`Failed to update: not found`)
+    }
+
     const entity = await this.genericRepository.preload({
       id,
       ...dto,
@@ -40,14 +85,14 @@ export abstract class CrudAbstractService<T extends BaseAbstractEntity> implemen
       throw new NotFoundException(`Failed to update: not found`)
     }
 
-    // @ts-expect-error @todo understand the type issue w/ typeorm here
+    // @ts-expect-error typeorm issue with DeepPartial and generics https://github.com/typeorm/typeorm/issues/2904
     await this.genericRepository.save(entity)
 
     return entity
   }
 
   async remove(identifier: number | string): Promise<T> {
-    const entity = await this.find(identifier)
+    const entity = await this.findOne(identifier)
 
     if (!entity) {
       throw new NotFoundException('Failed to delete: not found')
--- src/modules/database/types/crud-service.interface.ts
+++ src/modules/database/types/crud-service.interface.ts
@@ -2,7 +2,8 @@ import { DeepPartial } from 'typeorm'
 
 export interface CrudService<T> {
   findAll(): Promise<Array<T>>
-  find<ID extends number | string>(identifier: ID): Promise<T | undefined>
+  findOne<ID extends number | string>(identifier: ID): Promise<T | undefined>
+  find<ID extends Array<number> | Array<string>>(identifier: ID): Promise<Array<T>>
   update<ID extends number | string>(identifier: ID, dto: DeepPartial<T>): Promise<T>
   create(dto: DeepPartial<T>): Promise<T>
   remove<ID extends number | string>(identifier: ID): Promise<T>
